24be61fe2632fb4006cc76a80b55046af81720ab
Angelo <angelo@pigeonposse.com>
2025-06-05 16:32:34 +0200

    :bug: fix(env): fix check fn

--- packages/_example/src/routes/+layout.svelte
+++ packages/_example/src/routes/+layout.svelte
@@ -15,28 +15,6 @@
 
 <Utils />
 <Seo
-	jsonld={{ schema : {
-		'@type'           : 'BreadcrumbList',
-		'itemListElement' : [
-			{
-				'@type'    : 'ListItem',
-				'position' : 1,
-				'name'     : 'Books',
-				'item'     : 'https://example.com/books',
-			},
-			{
-				'@type'    : 'ListItem',
-				'position' : 2,
-				'name'     : 'Science Fiction',
-				'item'     : 'https://example.com/books/sciencefiction',
-			},
-			{
-				'@type'    : 'ListItem',
-				'position' : 3,
-				'name'     : 'Award Winners',
-			},
-		],
-	} }}
 	meta={{
 		title       : 'holña',
 		description : 'desc',
